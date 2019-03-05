dbb2957b461f301280d0a597e6066c73f9778c79
karlbjorneman <karl.bjorneman@gustaftech.se>
2019-03-05 09:04:18 +0100

    Added app bar and drawer

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,156 @@ import './App.css';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Route, Switch } from "react-router-dom";
-import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
+import { WithStyles, withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Board from './Board';
 import Login from './containers/Login'
 import Logout from './containers/Logout'
+import { AppBar, Toolbar, IconButton, ListItemIcon, ListItemText, List, Divider, ListItem, Hidden, Drawer } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-const styles = createStyles ({
+const drawerWidth = 240;
+
+const styles = (theme:any) => ({
   root: {
-    backgroundColor: 'red',
+    display: 'flex',
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing.unit * 3,
   },
 });
 
-interface IProps extends WithStyles<typeof styles> {}
+interface IAppProps extends WithStyles<typeof styles> {
+  theme: any;
+}
+interface IAppState {
+  mobileOpen: boolean;
+}
 
-class App extends React.Component<IProps> {
-  constructor(props: Readonly<IProps>) {
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: Readonly<IAppProps>) {
     super(props);
+
+    this.state = {
+      mobileOpen: false,
+    }
+
+    this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
   }
 
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: state.mobileOpen }));
+  };
+
   public render() {
+
+    const drawer = (
+      <div>
+        <div className={this.props.classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
     return (
       <React.Fragment>
-        <CssBaseline/>
-        <div className="App">
-          <header className="App-header">
-            <Typography component="h2" variant="h1" gutterBottom={true}>
-              Welcome to Notes
-            </Typography>
-          </header>
-          <div className={this.props.classes.root}>
-            <Switch>
-              <Route exact={true} path='/' component={Board}/>
-              <Route path='/login' component={Login} />
-              <Route path='/logout' component={Logout} />
-            </Switch>
+        <div className={this.props.classes.root}>
+          <CssBaseline/>
+            <AppBar position="fixed" className={this.props.classes.appBar}>
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="Open drawer"
+                  onClick={this.handleDrawerToggle}
+                  className={this.props.classes.menuButton}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Responsive drawer
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <nav className={this.props.classes.drawer}>
+              {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+              <Hidden smUp implementation="css">
+                <Drawer
+                  variant="temporary"
+                  anchor={this.props.theme.direction === 'rtl' ? 'right' : 'left'}
+                  open={this.state.mobileOpen}
+                  onClose={this.handleDrawerToggle}
+                  classes={{
+                    paper: this.props.classes.drawerPaper,
+                  }}
+                >
+                  {drawer}
+                </Drawer>
+              </Hidden>
+              <Hidden xsDown implementation="css">
+                <Drawer
+                  classes={{
+                    paper: this.props.classes.drawerPaper,
+                  }}
+                  variant="permanent"
+                  open
+                >
+                  {drawer}
+                </Drawer>
+              </Hidden>
+            </nav>
+            <main className={this.props.classes.content}>
+            <div className={this.props.classes.toolbar} />
+              <Switch>
+                <Route exact={true} path='/' component={Board}/>
+                <Route path='/login' component={Login} />
+                <Route path='/logout' component={Logout} />
+              </Switch>
+            </main>
           </div>
-        </div>
       </React.Fragment>
     );
   }
 }
 
-export default withStyles(styles)(App);
+export default withStyles(styles, { withTheme: true })(App);
